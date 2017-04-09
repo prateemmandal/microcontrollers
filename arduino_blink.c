5324046e521d0f90bda9779f089973b5430f767c
Prateem Mandal <prateem@gmail.com>
2017-04-08 17:37:59 -0700

    Added comments to the LED blink program.

--- arduino_blink.c
+++ arduino_blink.c
@@ -1,15 +1,20 @@
+// This program blinks the pins 2 and 7 and then pins 3 and 6 alternatively.
+// Add LEDs with 220 ohm resistors to these pins at one ed and to GND pin.
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main() {
-  // setup
-  // CAUTION: DO NOT set the last two pin direction. They are rx and tx pins and used to program arduino.
-  DDRD = DDRD | 0b11111110;
-  // loop
+  // Setup
+  // CAUTION: DO NOT set the last two pin direction. They are rx and tx pins
+  // and used to program arduino.
+  // Set pins 2 to 7 of bank D to output pins.
+  DDRD = DDRD | 0b11111100;
+  
+  // Loop
   while(1) {
-    PORTD = 0b10000100;
+    PORTD = 0b10000100; // set pins 2 and 7 and reset others.
     _delay_ms(200);
-    PORTD = 0b01001000;
+    PORTD = 0b01001000; // set pins 3 and 6 and reset others.
     _delay_ms(200);
   }
   return 0;
